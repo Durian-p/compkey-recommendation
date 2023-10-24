4160d3be2a52a04733e92e169d3a7f819c649889
wyxdrqcccc <865390641@qq,com>
2023-10-24 23:02:56 +0800

    更新test文件 hanlp因为py版本问题待定

--- preprocess/segment_test.py
+++ preprocess/segment_test.py
@@ -2,6 +2,8 @@ import time
 
 import segment_jieba
 import segment_snownlp
+from preprocess import segment_thulac, segment_pyltp
+
 
 def main():
     # 记录jieba开始结束时间，输出总耗时与it/s
@@ -23,6 +25,25 @@ def main():
     print("总耗时：", end_time - start_time, "s")
     print("it/s:", 21426941 / (end_time - start_time))
     print("--------------------------------------------")
+
+    start_time = time.time()
+    print("开始thulac分词")
+    segment_thulac.segmentKey()
+    end_time = time.time()
+    print("结束thulac分词")
+    print("总耗时：", end_time - start_time, "s")
+    print("it/s:", 21426941 / (end_time - start_time))
+    print("--------------------------------------------")
+
+    start_time = time.time()
+    print("开始pyltp分词")
+    segment_pyltp.segmentKey()
+    end_time = time.time()
+    print("结束pyltp分词")
+    print("总耗时：", end_time - start_time, "s")
+    print("it/s:", 21426941 / (end_time - start_time))
+    print("--------------------------------------------")
+
 
 
 if __name__ == '__main__':
---
+++ preprocess/setment_hanlp.py
@@ -0,0 +1,35 @@
+from pyhanlp import HanLP
+from tqdm import tqdm
+import os
+
+def segmentKey():
+    input_dir = "../dataset/result/queries.result"
+    output_dir_prefix = "../dataset/result/hanlp/normal_segmented_"
+    keywords = []
+    output_dirs = []
+    with open("keyword.txt", "r", encoding="utf8") as keyword_file, open(input_dir, "r", encoding="utf8") as input_file:
+        for line in keyword_file:
+            keyword = line.strip()
+            HanLP.Config.CustomDictionary.add(keyword)  # 添加用户自定义词汇
+            keywords.append(keyword)
+            output_dirs.append(output_dir_prefix + keyword + ".result")
+
+        # 检查文件是否存在或不为空
+        for output_file in output_dirs:
+            if os.path.exists(output_file) and os.path.getsize(output_file) > 0:
+                with open(output_file, 'w', encoding="utf8") as output_file:
+                    # 清空文件内容
+                    output_file.truncate()
+
+        # 寻找关键字并分词
+        for line in tqdm(input_file, desc="Processing", ncols=100):
+            for keyword in keywords:
+                if keyword in line.strip():
+                    word_list = HanLP.segment(line.strip())  # 使用HanLP进行分词
+                    word_list = [term.word for term in word_list]  # 获取分词结果中的词语部分
+                    with open(output_dir_prefix + keyword + ".result", "a", encoding="utf8") as output_file:
+                        output_file.write(" ".join(word_list) + "\n")  # 写入换行符保持原始行结构
+
+
+if __name__ == '__main__':
+    segmentKey()
